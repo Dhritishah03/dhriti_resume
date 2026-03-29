8b384caedbe30f3c4286a7edbe163e2942f5133a
Dhritishah03 <113230308+Dhritishah03@users.noreply.github.com>
2026-03-29 14:52:28 +0530

    Configure Next.js for production and export settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/YOUR_REPO_NAME" : "",
+  assetPrefix: isProd ? "/YOUR_REPO_NAME/" : "",
 };
 
 export default nextConfig;
